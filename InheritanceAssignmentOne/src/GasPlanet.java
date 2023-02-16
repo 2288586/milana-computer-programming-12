be9f38925c1dcd921ef019d6ecf78fea1e336889
2288586 <2288586@learn.vsb.bc.ca>
2023-02-16 10:53:42 -0800

    Added "GasPlanet" and "RockPlanet" Methods
    Added methods to "GasPlanet" and "RockPlanet" classes.

--- InheritanceAssignmentOne/src/GasPlanet.java
+++ InheritanceAssignmentOne/src/GasPlanet.java
@@ -1,2 +1,34 @@
-public class GasPlanet {
+import java.awt.Color;
+
+public class GasPlanet extends Planet {
+    private boolean hasRings;
+    private Color color;
+
+    public GasPlanet(int orbitTime, String designation, boolean hasRings, Color color) {
+        super(orbitTime, designation);
+
+        this.hasRings = hasRings;
+        this.color = color;
+    }
+
+    public boolean isHasRings() {
+        return hasRings;
+    }
+
+    public void setHasRings(boolean hasRings) {
+        this.hasRings = hasRings;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Gas Planet Designation: '" + this.getDesignation() + "'";
+    }
 }
--- InheritanceAssignmentOne/src/RockPlanet.java
+++ InheritanceAssignmentOne/src/RockPlanet.java
@@ -1,2 +1,22 @@
-public class RockPlanet {
+public class RockPlanet extends Planet {
+    private boolean habitable;
+
+    public RockPlanet(int orbitTime, String designation, boolean habitable) {
+        super(orbitTime, designation);
+
+        this.habitable = habitable;
+    }
+
+    public boolean isHabitable() {
+        return habitable;
+    }
+
+    public void setHabitable(boolean habitable) {
+        this.habitable = habitable;
+    }
+
+    @Override
+    public String toString() {
+        return "Rock Planet Designation: '" + this.getDesignation() + "'";
+    }
 }
